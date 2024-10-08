76124e3a85a2ec9d6a1a03ceec4c87c2b69cbf56
Scott Nicolas <scottnicolas@sub74-171.uhh.hawaii.edu>
2024-10-07 19:09:50 -1000

    initiate supabase database

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,8 @@ import { HiHome } from "react-icons/hi";
 import { twMerge } from "tailwind-merge";
 import { BiSearch } from "react-icons/bi";
 
+import Button from "./Button";
+
 interface HeaderProps {
   children: React.ReactNode;
   className?: string;
@@ -48,7 +50,25 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             <BiSearch className="text-black" size={20} />
           </button>
         </div>
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+            <div>
+              <Button
+                onClick={() => {}}
+                className="bg-transparent text-neutral-300 font-medium"
+              >
+                Sign up
+              </Button>
+            </div>
+            <div>
+              <Button onClick={() => {}} className="bg-white px-6 py-2">
+                Log in
+              </Button>
+            </div>
+          </>
+        </div>
       </div>
+      {children}
     </div>
   );
 };
