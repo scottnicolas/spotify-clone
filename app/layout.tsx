b8ad1c3929a0c984347647f9d2b4333e52d5f00c
Scott Nicolas <scottnicolas@Scotts-MacBook-Pro.local>
2024-10-07 23:53:27 -1000

    add authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import "./globals.css";
+
 import Sidebar from "@/components/Sidebar";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
 
 const font = Figtree({
   subsets: ["latin"],
@@ -20,7 +24,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${font.className} antialiased`}>
-        <Sidebar>{children}</Sidebar>
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider />
+            <Sidebar>{children}</Sidebar>
+          </UserProvider>
+        </SupabaseProvider>
       </body>
     </html>
   );
---
+++ components/AuthModal.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import {
+  useSessionContext,
+  useSupabaseClient,
+} from "@supabase/auth-helpers-react";
+import { useRouter } from "next/navigation";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+
+import Modal from "./Modal";
+
+import useAuthModal from "@/hooks/useAuthModal";
+
+const AuthModal = () => {
+  const supabaseClient = useSupabaseClient();
+  const router = useRouter();
+  const { session } = useSessionContext();
+  const {} = useAuthModal();
+
+  return (
+    <Modal
+      title="Welcome back"
+      description="Login to your account"
+      isOpen
+      onChange={() => {}}
+    >
+      <Auth
+        theme="dark"
+        magicLink
+        providers={["github", "google", "apple"]}
+        supabaseClient={supabaseClient}
+        appearance={{
+          theme: ThemeSupa,
+          variables: {
+            default: {
+              colors: {
+                brand: "#404040",
+                brandAccent: "#22c55e",
+              },
+            },
+          },
+        }}
+      />
+    </Modal>
+  );
+};
+
+export default AuthModal;
