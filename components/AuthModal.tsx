49bd88abaa0d385e54eec8bc8366e1cf300d1e11
Scott Nicolas <scottnicolas@sub69-7.uhh.hawaii.edu>
2024-10-10 12:58:10 -1000

    finish up authentication modal functionality

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -7,6 +7,7 @@ import {
 import { useRouter } from "next/navigation";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useEffect } from "react";
 
 import Modal from "./Modal";
 
@@ -16,19 +17,32 @@ const AuthModal = () => {
   const supabaseClient = useSupabaseClient();
   const router = useRouter();
   const { session } = useSessionContext();
-  const {} = useAuthModal();
+  const { onClose, isOpen } = useAuthModal();
+
+  useEffect(() => {
+    if (session) {
+      router.refresh();
+      onClose();
+    }
+  }, [session, router, onClose]);
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
+    }
+  };
 
   return (
     <Modal
       title="Welcome back"
       description="Login to your account"
-      isOpen
-      onChange={() => {}}
+      isOpen={isOpen}
+      onChange={onChange}
     >
       <Auth
         theme="dark"
         magicLink
-        providers={["github", "google", "apple"]}
+        providers={["github"]}
         supabaseClient={supabaseClient}
         appearance={{
           theme: ThemeSupa,
